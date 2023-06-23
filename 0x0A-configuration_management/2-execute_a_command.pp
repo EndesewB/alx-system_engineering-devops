7c1787191e66a4996abe1300ae67c5d456a1a018
endesew bantayehu <endesew12@gmail.com>
2023-06-23 12:37:34 -0700

    configuration management

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # To create a Puppet manifest that kills a process named\
 # "killmenow" using the exec resource and pkill,
 
-exec { 'killmenow':
-  command  => 'pkill -f killmenow',
-  path  => ['/usr/bin:/bin']
+exec { 'kill':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
 }
