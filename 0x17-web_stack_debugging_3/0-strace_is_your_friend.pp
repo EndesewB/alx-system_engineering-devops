7abae700f1160200315e3240bfd775330f5c6ef7
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-08-10 11:02:11 +0300

    Updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,8 @@
 # Declare the exec resource to fix the issue
 exec { 'fix-apache':
   command => '/usr/bin/apt-get update && /usr/bin/apt-get install -y php-module-name',
-  onlyif  => '/usr/bin/dpkg --compare-versions $(/usr/bin/dpkg-query --showformat=\'${Version}\' --show php-module-name) ne desired_version',
+  onlyif  => '/usr/bin/dpkg --compare-versions $(/usr/bin/dpkg-query 
+  --showformat=\'${Version}\' --show php-module-name) ne desired_version',
 }
 
 # Notify the service to restart if the package is updated
